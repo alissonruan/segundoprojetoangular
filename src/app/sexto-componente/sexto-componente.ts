1215ed20de65c041ea68fef1a428496f3b5c4635
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-09-16 22:10:03 -0300

    feat: add function forms

--- src/app/sexto-componente/sexto-componente.ts
+++ src/app/sexto-componente/sexto-componente.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { min } from 'rxjs';
+import { Pessoa } from '../modelo/Pessoa';
 
 @Component({
   selector: 'app-sexto-componente',
-  imports: [FormsModule, CommonModule, FormGroup, ReactiveFormsModule, FormControl],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './sexto-componente.html',
   styleUrl: './sexto-componente.css'
 })
@@ -17,4 +17,38 @@ export class SextoComponente {
     idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
   });
+
+  //Visibilidade dos botões
+  btnCadastrar: boolean = true;
+
+  //Cadastrar Pessoa
+  vetor:Pessoa[];
+
+  //Função Cadastrar
+  Cadastrar(){
+    //Inclusão da função no vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    //Limpeza dos inputs
+    this.formulario.reset();
+
+    //Visualização
+    console.table(this.vetor);}
+
+  //Armazenar indice da Pessoa selecionada
+  indice: number = -1;
+  
+  Selecionar(indice:number){
+    //atribuir o indice da pessoa
+    this.indice = indice;
+
+    //Atribuir os dados da pessoa no formulario
+    this.formulario.setValue({
+      nome:this.vetor[indice].nome,
+      idade:this.vetor[indice].idade,
+      cidade:this.vetor[indice].cidade
+    });
+
+    this.btnCadastrar = false;
+  }
 }
